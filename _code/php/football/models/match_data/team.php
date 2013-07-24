2dbafe6e577fd7b95527af19568c93e2126cc8c1
hx <peoplebike@gmail.com>
2013-07-24 17:26:05 +0800

    修改了team.php返回的JSON数据。 注意：之前数据库有很多比赛主队和客队排名都是0，是我之前录入数据的时候搞出来的BUG。因此需要把这些比赛的主队客队排名重新生成一下，可以用 UPDATE matches set home_rank=8,away_rank=11 where home_rank=0 or away_rank=0 把这样的比赛都设定设成主队排第8，客队排第11

--- _code/php/football/models/match_data/team.php
+++ _code/php/football/models/match_data/team.php
@@ -3,47 +3,53 @@ require_once BASEPATH.'db/connect.php';
 function matches_result($team_name){
 	$mysqli=load_db();
 	$team_name=$mysqli->real_escape_string($team_name);
-	echo $team_name;
-	$query="SELECT m.round,m.home_rank,m.away_rank,m.home_goal,m.away_goal,home.short_name AS home_name, away.short_name AS away_name, m.date AS date, p.full_name AS player_name, g.time AS goal_time
-	        FROM matches m
-		INNER JOIN team home ON m.home_team = home.tid
-		INNER JOIN team away ON m.away_team = away.tid
-		LEFT OUTER JOIN goals g ON g.mid = m.mid
-		INNER JOIN players p ON p.pid = g.pid
-		WHERE home.short_name = 'man utd'
-		OR away.short_name = '$team_name'
-		ORDER BY m.round,g.time";
-
-	$result=$mysqli->query($query);
+	$query="SELECT  m.home_rank,
+			m.away_rank,
+			m.home_point,
+			m.away_point,
+			m.home_goal,
+			m.away_goal,
+			m.date,
+			home.short_name AS home_name,
+			away.short_name AS away_name
+		FROM matches m
+		INNER JOIN team home
+			ON m.home_team = home.tid
+		INNER JOIN team away
+			ON m.away_team = away.tid
+		WHERE home.short_name = '$team_name'
+			OR away.short_name = '$team_name'
+			ORDER BY m.date ASC";
+	$result = $mysqli->query($query);
 	if($mysqli->errno){
-		echo "path.php function matches_result error\n";
+		echo "team.php failed in query\n";
 		echo $mysqli->error;
 		exit();
 	}
-	//var_dump($result->fetch_object());
-	$matches=array();
-	while($match_per_goal=$result->fetch_object()){
-		$round=$match_per_goal->round;
-		$player_name=$match_per_goal->player_name;
-		$goal_time = $match_per_goal->goal_time;
-		if(!isset($matches[$round])){
-			$home_name=$match_per_goal->home_name;
-			$away_name=$match_per_goal->away_name;
-			$home_goal=$match_per_goal->home_goal;
-			$away_goal=$match_per_goal->away_goal;
-			$home_rank=$match_per_goal->home_rank;
-			$away_rank=$match_per_goal->away_rank;
-			$date=$match_per_goal->date;
-			$matches[$round]=array("home"=>$home_name, "away"=>$away_name, "home_goal"=>$home_goal, "away_goal"=>$away_goal,"home_rank"=>$home_rank, "away_rank"=>$away_rank, "date"=>$date);
-			$matches[$round]["goals"]=array();
-			array_push($matches[$round]["goals"],array("player"=>$player_name,"time"=>$goal_time));
-		}else{
-			array_push($matches[$round]["goals"],array("player"=>$player_name,"time"=>$goal_time));
-		}
+	$matches = array();
+	while($match = $result->fetch_object()){
+		if($team_name==$match->home_name){
+			$ishome = 1;
+			$rank = $match->home_rank;
+			$point = $match->home_point;
+			$opponent = $match->away_name;
+		 }else{
+			 $ishome = 0;
+			 $rank = $match->away_rank;
+			 $point = $match->away_point;
+			 $opponent = $match->home_name;
+		 }
+		$score = "$match->home_goal : $match->away_goal";
+		$date = $match->date;
+		$e=array("rank"=>$rank, "score"=>$score, "opponent"=>$opponent, "ishome"=>$ishome, "date"=>$date, "point"=>$point);
+		array_push($matches,$e);
 	}
-	//var_dump($matches);
-	$matches=array_values($matches);
-	$json["data"]=$matches;
+	$json["data"] = $matches;
 	return json_encode($json);
 }
+//$team_name = 'arsenal';
+//define("BASEPATH", "/home/bran/projects/football/");
+//require_once BASEPATH.'db/connect.php';
+//echo matches_result($team_name);
 ?>
+
